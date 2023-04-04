dc31aff5891f9a88cf19c1106b78fadec2926650
RadimYassin <radimyassin20@gamil.com>
2023-04-04 23:10:13 +0200

    create routes

---
+++ routes/routes.js
@@ -0,0 +1,60 @@
+const express=require("express")
+
+const Route=express.Router
+const Blog = require('../models/blog');
+
+
+  
+  // blog routes
+  Route.get('/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
+  
+  Route.get('/', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  Route.post('/', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+  
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  Route.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+
+  module.exports=Route;
